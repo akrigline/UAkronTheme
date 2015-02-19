eb561b7a344526b0a69b85f8d6e91a49547fde7d
akrigline <andrew@akrigline.com>
2015-02-19 18:57:30 -0500

    rouge container and section

--- info.php
+++ info.php
@@ -15,7 +15,7 @@ get_header(); ?>
 		<?php if ( have_posts() ) : ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'content', get_post_format() ); ?>
+				<?php get_template_part( 'content' ); ?>
 
 			<?php endwhile; ?>
 
@@ -58,5 +58,6 @@ get_header(); ?>
   		</div>
 		</div>
   </div><!-- quickStats -->
-  
+</div>
+</section>
 <?php get_footer(); ?>
